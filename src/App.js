3a9cc122633ac670531cdf2e02fe2ecabd929023
Zac Valant <zac23.v@gmail.com>
2024-07-30 06:09:56 -0500

    Feature Implementation: added all charts and dashboard with routing functionality complete.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
-import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+import Team from "./scenes/team";
+import Contacts from "./scenes/contacts";
+import Invoices from "./scenes/invoices";
+import Form from "./scenes/form";
+import Calendar from "./scenes/calendar/index";
+import FAQ from "./scenes/faq";
+import Bar from "./scenes/bar";
+import Pie from "./scenes/pie";
+import Line from "./scenes/line";
+import Geography from "./scenes/geography";
+
+import { CssBaseline, ThemeProvider, Box } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
-import Dashboard from "./scenes/dashboard"
-import Sidebar from "./scenes/global/Sidebar"
+
 function App() {
 
   const [theme, colorMode] = useMode();
@@ -20,6 +32,16 @@ function App() {
             <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
               <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie/>}/>
+              <Route path="/line" element={<Line />} />
+              <Route path="/geography" element={<Geography />} />
             </Routes>
           </Box>
           
